ef8fc132bb19b484062cca37d00f2b91875fe506
smittey <andy_smithy@hotmail.co.uk>
2020-03-22 21:53:55 +0000

    Updating GA code to be correct for site

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,29 +48,25 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        icon: 'src/assets/images/a-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/a-icon.png',
       },
     },
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-74354041-1',
+        trackingId: 'UA-74354041-2',
       },
     },
-
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images-contentful`,
+            resolve: 'gatsby-remark-images-contentful',
             options: {
               withWebp: true,
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
               maxWidth: 1000,
             },
           },
@@ -80,56 +76,5 @@ module.exports = {
         ],
       },
     },
-
-
-    // {
-    //   resolve: `gatsby-transformer-remark`,
-    //   options: {
-    //     plugins: [
-    //       {
-    //         resolve: `gatsby-remark-prismjs`,
-    //         options: {
-    //           classPrefix: "language-",
-    //           inlineCodeMarker: null,
-
-    //           aliases: {},
-    //           showLineNumbers: true,
-    //           // If setting this to true, the parser won't handle and highlight inline
-    //           // code used in markdown i.e. single backtick code like `this`.
-    //           noInlineHighlight: false,
-    //           // This adds a new language definition to Prism or extend an already
-    //           // existing language definition. More details on this option can be
-    //           // found under the header "Add new language definition or extend an
-    //           // existing language" below.
-    //           languageExtensions: [
-    //             {
-    //               language: "superscript",
-    //               extend: "javascript",
-    //               definition: {
-    //                 superscript_types: /(SuperType)/,
-    //               },
-    //               insertBefore: {
-    //                 function: {
-    //                   superscript_keywords: /(superif|superelse)/,
-    //                 },
-    //               },
-    //             },
-    //           ],
-    //           // Customize the prompt used in shell output
-    //           // Values below are default
-    //           prompt: {
-    //             user: "root",
-    //             host: "localhost",
-    //             global: false,
-    //           },
-    //           // By default the HTML entities <>&'" are escaped.
-    //           // Add additional HTML escapes by providing a mapping
-    //           // of HTML entities and their escape value IE: { '}': '&#123;' }
-    //           escapeEntities: {},
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
   ],
 };
